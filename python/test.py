81b1e2e0d415aedc1186ae6706b74e86bd6c112e
Luca Pescatore <luca.pescatore@cern.ch>
2017-12-14 12:33:04 +0000

    Update test.py

--- python/test.py
+++ python/test.py
@@ -1,19 +1,18 @@
-
-print "Testing"
+print ("Testing")
 import sys
 
 f = open('log','w')
 
 f.write(sys.version_info.__repr__())
 
-print "Looking for libraries"
+print ("Looking for libraries")
 import pyutils
 import ROOT
 
 f.write('\npyutils and ROOT imported ok\n')
 f.write('Trying to read some files from eos\n')
 
-print "Looking for files"
+print ("Looking for files")
 import B2KTauMu as an
 files = an.utils.remote_ls_fromids(an.dataids['CL11'])
 
